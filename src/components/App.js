322b6f7790761ab0e552528478863b9fea28c969
Anesti Gjikoka <anesti.gjikoka@gmail.com>
2020-03-09 09:08:30 +0100

    Update React Pics Project

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 
 class App extends React.Component {
 
-    onSearchSubmit(term) {
-        console.log(term);
-    }
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+            headers: {
+                Authorization: 'Client-ID f-TxoeLfRDimvES_dP36TA36cYwXlxGz_epKQt0S0eQ'  
+            }
+        });
+
+        console.log(response.data.results);
+    } 
 
     render() {
         return (
